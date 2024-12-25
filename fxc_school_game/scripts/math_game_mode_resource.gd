67bf437ccc85c3917c80ce087afe3307dcf2b435
Benjamin Brieber <bbrieber@users.noreply.github.com>
2024-12-25 16:15:58 +0100

    new Scripts

---
+++ fxc_school_game/scripts/math_game_mode_resource.gd
@@ -0,0 +1,6 @@
+class_name MathGameModeResource extends Resource
+
+@export_flags("Addition","Subtraction","Multiplication","Division") var allowed_modes:int = 0
+
+@export var max_number:int = 10
+@export var cap_max:bool=true
